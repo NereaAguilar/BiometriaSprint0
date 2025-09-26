c44e5d6e758aca6015b61ba7fd14f8a041defea8
NereaAguilarFores <nagufor@upv.edu.es>
2025-09-26 02:07:40 +0200

    Cambios Prueba y Ingenieria Inversa Java

--- src/arduino/HolaMundoIBeacon/HolaMundoIBeacon.ino
+++ src/arduino/HolaMundoIBeacon/HolaMundoIBeacon.ino
@@ -40,7 +40,6 @@ namespace Globales {
 #include "Publicador.h"
 #include "Medidor.h"
 
-
 // --------------------------------------------------------------
 // --------------------------------------------------------------
 namespace Globales {
