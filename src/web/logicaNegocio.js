c4addad03f7cfb792c6788de63200e5de0cce91e
NereaAguilarFores <nagufor@upv.edu.es>
2025-10-07 03:27:50 +0200

    Diseños Mejorados
    Diseño Api y logica de servidor mejorado v4
    Diseño WEB y Logica servidor subido
    Cambio en logicaNegocio.js, mejorado para que muestre el nombre del tipo de medición.

--- src/web/logicaNegocio.js
+++ src/web/logicaNegocio.js
@@ -1,32 +1,41 @@
 const API = "https://nagufor.upv.edu.es/app.php";
 
+//Obtenemos la última medida para mostrarla en la web
 async function obtenerUltima() {
     const div = document.getElementById("resultado");
+
     try {
         const res = await fetch(API + "/medida/ultima");
+
         if (!res.ok) {
             div.innerHTML = "No hay medidas en la base de datos.";
             return;
         }
+
         const data = await res.json();
+		let tipoMedida;
+		
+        if (data.tipo === 11) {
+            tipoMedida = "CO₂";
+        } else if (data.tipo === 12) {
+            tipoMedida = "Temperatura";
+        } else {
+            tipoMedida = data.tipo;
+        }
+
         div.innerHTML = `
             <b>ID:</b> ${data.id}<br>
-            <b>Tipo:</b> ${nombreTipo(data.tipo)}<br>
+            <b>Tipo:</b> ${tipoMedida}<br>
             <b>Medición:</b> ${data.medicion}<br>
             <b>Fecha:</b> ${data.fecha}
         `;
+
     } catch (err) {
         div.innerHTML = "Error al conectar con la API.";
     }
 }
 
-function nombreTipo(tipo) {
-    if (tipo === 11) return "CO₂ (ppm)";
-    if (tipo === 12) return "Temperatura (°C)";
-    if (tipo === 13) return "Ruido (dB)";
-    return "Desconocido";
-}
-
+//Carga cada 5 segundos
 window.onload = () => {
     obtenerUltima();
     setInterval(obtenerUltima, 5000);
